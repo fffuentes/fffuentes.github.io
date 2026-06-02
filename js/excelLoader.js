14a0423540884372eca5a116ad2a351f87fa1681
fffuentes <30441095+fffuentes@users.noreply.github.com>
2026-06-02 16:36:48 -0600

    Update excelLoader.js

--- js/excelLoader.js
+++ js/excelLoader.js
@@ -79,10 +79,59 @@ function procesarResumen(){
             }
         );
 
-    console.log(
-        "Resumen",
-        datos
-    );
+    dashboardData.resumen = {
+
+    rotacionGlobal:
+        datos[0][1],
+
+    clasificacion:
+        datos[0][2],
+
+    skuTotal:
+        datos[2][2],
+
+    skuRotacion: [
+
+        datos[3][1],
+        datos[4][1],
+        datos[5][1],
+        datos[6][1]
+
+    ],
+
+    inventarioRotacion: [
+
+        datos[8][1],
+        datos[9][1],
+        datos[10][1],
+        datos[11][1]
+
+    ],
+
+    antiguedadUso: [
+
+        datos[13][1],
+        datos[14][1],
+        datos[15][1],
+        datos[16][1]
+
+    ],
+
+    antiguedadGTQ: [
+
+        datos[18][1],
+        datos[19][1],
+        datos[20][1],
+        datos[21][1]
+
+    ]
+
+};
+
+console.log(
+    "Resumen procesado",
+    dashboardData.resumen
+);
 
 }
 
@@ -104,9 +153,64 @@ function procesarMRP(){
             }
         );
 
-    console.log(
-        "MRP",
-        datos
-    );
+   dashboardData.mrp = {
+
+    rotacionGlobal:
+        datos[0][1],
+
+    clasificacion:
+        datos[0][2],
+
+    consumoAnual:
+        datos[1][1],
+
+    inventario:
+        datos[2][1],
+
+    skuTotal:
+        datos[3][2],
+
+    skuRotacion: [
+
+        datos[4][1],
+        datos[5][1],
+        datos[6][1],
+        datos[7][1]
+
+    ],
+
+    inventarioRotacion: [
+
+        datos[9][1],
+        datos[10][1],
+        datos[11][1],
+        datos[12][1]
+
+    ],
+
+    antiguedadUso: [
+
+        datos[14][1],
+        datos[15][1],
+        datos[16][1],
+        datos[17][1]
+
+    ],
+
+    antiguedadGTQ: [
+
+        datos[19][1],
+        datos[20][1],
+        datos[21][1],
+        datos[22][1]
+
+    ]
+
+};
+
+console.log(
+    "MRP procesado",
+    dashboardData.mrp
+);
 
 }
